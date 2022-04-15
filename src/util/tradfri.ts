d5487ce9468d63f7cf9a2a327e631526e7bf530a
typesafety <ailoodee@gmail.com>
2022-04-15 18:19:23 +0200

    Validate setLight inputs

--- src/util/tradfri.ts
+++ src/util/tradfri.ts
@@ -20,24 +20,64 @@ export class TradfriCli {
     // singleton object. only one connection possible. See getInstance.
     private constructor() { }
 
-
-    //config values: 
-    //    onOff : bool
-    //    dimmer : 0-100
-    //    color : hexcode (example F12C3B)
-    //    transitionTime : milliseconds
-    setLight = async (light: any, config: ILightConfig) => {
-
-        // light level capped at .8 to preserve lifetime and since it's annoyingly bright.
-        const outConf = {
-            onOff: config.lightsOn,
-            dimmer: config.dimmerLevel * 0.8,
-            color: config.color,
-            transitionTime: config.transitionTime
+    /**
+     * Universal function for setting the light of a bulb, given an ILightConfig object.
+     *
+     * config values:
+     *     onOff : bool
+     *     dimmer : 0-100
+     *     color : hexcode (example F12C3B)
+     *     transitionTime : milliseconds
+     */
+    setLight = async (light: any, config: Partial<ILightConfig>) => {
+        type PLC = Partial<ILightConfig>
+        function addDimmer(level: number, confBuilder: PLC): PLC {
+            if (level <= 0 || level > 100) {
+                throw Error(`dimmerLevel out of bounds: ${config.dimmerLevel},` +
+                            ` must be in range (0, 100]`)
+            }
+            // Light level capped at 80 % to preserve lifetime and because it's
+            // annoyingly bright.
+            return {...confBuilder, dimmerLevel: level * 0.8}
+        }
+        function addColor(color: string, confBuilder: PLC): PLC {
+            if (color.length !== 6) {
+                throw Error(`color has an invalid length: ${color.length}, must be 6`)
+            } else if (color.match(/[a-zA-Z0-9]{6}/)) {  // Match a 6 character hex code
+                throw Error(`color is not a valid hex code: ${color}`)
+            }
+            return {...confBuilder, color: color}
+        }
+        function addTransitionTime(time: number, confBuilder: PLC): PLC {
+            if (time < 0 || time > (10 ^ 5)) {
+                throw Error(`Invalid transitionTime: ${time}, must be in the range [0, 10000]`)
+            }
+            return {...confBuilder, transitionTime: time}
         }
 
-        await this.cli.operateLight(light, outConf, false) //always use operateLight
-        return
+        // Collect and validate inputs that are present.
+        let builder: PLC = {}
+        if (config.lightsOn !== undefined) {
+            builder = {...builder, lightsOn: config.lightsOn}
+        }
+        if (config.dimmerLevel !== undefined) {
+            builder = addDimmer(config.dimmerLevel, builder)
+        }
+        if (config.color !== undefined) {
+            builder = addColor(config.color, builder)
+        }
+        if (config.transitionTime !== undefined) {
+            builder = addTransitionTime(config.transitionTime, builder)
+        }
+
+        // Construct the object to send to the external tradfri API.
+        const outConf = {
+            onOff: config.lightsOn,
+            dimmer: config.dimmerLevel,
+            color: config.color,
+            transitionTime: config.transitionTime,
+        }
+        await this.cli.operateLight(light, outConf, false)  // always use operateLight
     }
 
     getBulbs = () => {
